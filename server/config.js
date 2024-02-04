10b6696509cc071355b37de16e903dc203d59e45
Steve Hoang <82992510+lotusk08@users.noreply.github.com>
2024-02-04 10:33:31 +0700

    update new data

--- server/config.js
+++ server/config.js
@@ -17,48 +17,48 @@ const prizes = [
     type: 1,
     count: 3,
     text: "Giải đặc biệt",
-    title: "50.000.000 VNĐ",
-    img: "../img/secrit.jpg"
+    title: "50.000.000",
+    img: "../img/dac biet.webp"
   },
   {
     type: 2,
     count: 5,
     text: "Giải nhất",
-    title: "40.000.000 VNĐ",
-    img: "../img/mbp.jpg"
+    title: "40.000.000",
+    img: "../img/nhat.webp"
   },
   {
     type: 3,
     count: 5,
     text: "Giải nhì",
-    title: "30.000.000 VNĐ",
-    img: "../img/huawei.png"
+    title: "30.000.000",
+    img: "../img/nhi.webp"
   },
   {
     type: 4,
     count: 10,
     text: "Giải ba",
-    title: "20.000.000 VNĐ",
-    img: "../img/ipad.jpg"
+    title: "20.000.000",
+    img: "../img/ba.webp"
   },
   {
     type: 5,
     count: 30,
     text: "Giải khuyến khích",
-    title: "10.000.000 VNĐ",
-    img: "../img/spark.jpg"
+    title: "10.000.000",
+    img: "../img/kk.webp"
   }
 ];
 
 /**
  * The number of prizes for one time corresponds to Prizes
  */
-const EACH_COUNT = [1, 1, 5, 6, 7, 8, 9, 10];
+const EACH_COUNT = [1, 3, 5, 5, 5, 10];
 
 /**
  * Card company name identification
  */
-const COMPANY = "We are IN";
+const COMPANY = "IN";
 
 module.exports = {
   prizes,
